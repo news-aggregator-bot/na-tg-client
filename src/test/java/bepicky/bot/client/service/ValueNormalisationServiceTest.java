d59868db5034fb70962def4b2d2bbaafcac7c715
v.yemelianov <v.yemelianov@voverc.com>
2022-01-13 23:04:52 +0000

    date time value normaliser

--- src/test/java/bepicky/bot/client/service/ValueNormalisationServiceTest.java
+++ src/test/java/bepicky/bot/client/service/ValueNormalisationServiceTest.java
@@ -21,17 +21,17 @@ class ValueNormalisationServiceTest {
         return Stream.of(
             Arguments.of(new Date(100500), "01 Jan 1970"),
             Arguments.of(new Date(1642075786169L), "13 Jan 2022"),
-            Arguments.of(new Date(1639094400000L), "10 Dec 2021"),
-            Arguments.of(new Date(1636502400000L), "10 Nov 2021"),
-            Arguments.of(new Date(1633820400000L), "10 Oct 2021"),
-            Arguments.of(new Date(1631228400000L), "10 Sep 2021"),
-            Arguments.of(new Date(1628550000000L), "10 Aug 2021"),
-            Arguments.of(new Date(1625871600000L), "10 Jul 2021"),
-            Arguments.of(new Date(1623279600000L), "10 Jun 2021"),
-            Arguments.of(new Date(1620601200000L), "10 May 2021"),
-            Arguments.of(new Date(1618009200000L), "10 Apr 2021"),
-            Arguments.of(new Date(1615334400000L), "10 Mar 2021"),
-            Arguments.of(new Date(1612915200000L), "10 Feb 2021")
+            Arguments.of(new Date(1639137600000L), "10 Dec 2021"),
+            Arguments.of(new Date(1636545600000L), "10 Nov 2021"),
+            Arguments.of(new Date(1633863600000L), "10 Oct 2021"),
+            Arguments.of(new Date(1631271600000L), "10 Sep 2021"),
+            Arguments.of(new Date(1628593200000L), "10 Aug 2021"),
+            Arguments.of(new Date(1625914800000L), "10 Jul 2021"),
+            Arguments.of(new Date(1623322800000L), "10 Jun 2021"),
+            Arguments.of(new Date(1620644400000L), "10 May 2021"),
+            Arguments.of(new Date(1618052400000L), "10 Apr 2021"),
+            Arguments.of(new Date(1615377600000L), "10 Mar 2021"),
+            Arguments.of(new Date(1612958400000L), "10 Feb 2021")
         );
     }
 
@@ -47,17 +47,17 @@ class ValueNormalisationServiceTest {
         return Stream.of(
             Arguments.of(new Date(100500), "01 янв. 1970"),
             Arguments.of(new Date(1642075786169L), "13 янв. 2022"),
-            Arguments.of(new Date(1639094400000L), "10 дек. 2021"),
-            Arguments.of(new Date(1636502400000L), "10 нояб. 2021"),
-            Arguments.of(new Date(1633820400000L), "10 окт. 2021"),
-            Arguments.of(new Date(1631228400000L), "10 сент. 2021"),
-            Arguments.of(new Date(1628550000000L), "10 авг. 2021"),
-            Arguments.of(new Date(1625871600000L), "10 июл. 2021"),
-            Arguments.of(new Date(1623279600000L), "10 июн. 2021"),
-            Arguments.of(new Date(1620601200000L), "10 мая 2021"),
-            Arguments.of(new Date(1618009200000L), "10 апр. 2021"),
-            Arguments.of(new Date(1615334400000L), "10 мар. 2021"),
-            Arguments.of(new Date(1612915200000L), "10 февр. 2021")
+            Arguments.of(new Date(1639137600000L), "10 дек. 2021"),
+            Arguments.of(new Date(1636545600000L), "10 нояб. 2021"),
+            Arguments.of(new Date(1633863600000L), "10 окт. 2021"),
+            Arguments.of(new Date(1631271600000L), "10 сент. 2021"),
+            Arguments.of(new Date(1628593200000L), "10 авг. 2021"),
+            Arguments.of(new Date(1625914800000L), "10 июл. 2021"),
+            Arguments.of(new Date(1623322800000L), "10 июн. 2021"),
+            Arguments.of(new Date(1620644400000L), "10 мая 2021"),
+            Arguments.of(new Date(1618052400000L), "10 апр. 2021"),
+            Arguments.of(new Date(1615377600000L), "10 мар. 2021"),
+            Arguments.of(new Date(1612958400000L), "10 февр. 2021")
         );
     }
 
@@ -73,17 +73,17 @@ class ValueNormalisationServiceTest {
         return Stream.of(
             Arguments.of(new Date(100500), "01 січ. 1970"),
             Arguments.of(new Date(1642075786169L), "13 січ. 2022"),
-            Arguments.of(new Date(1639094400000L), "10 груд. 2021"),
-            Arguments.of(new Date(1636502400000L), "10 лист. 2021"),
-            Arguments.of(new Date(1633820400000L), "10 жовт. 2021"),
-            Arguments.of(new Date(1631228400000L), "10 вер. 2021"),
-            Arguments.of(new Date(1628550000000L), "10 серп. 2021"),
-            Arguments.of(new Date(1625871600000L), "10 лип. 2021"),
-            Arguments.of(new Date(1623279600000L), "10 черв. 2021"),
-            Arguments.of(new Date(1620601200000L), "10 трав. 2021"),
-            Arguments.of(new Date(1618009200000L), "10 квіт. 2021"),
-            Arguments.of(new Date(1615334400000L), "10 бер. 2021"),
-            Arguments.of(new Date(1612915200000L), "10 лют. 2021")
+            Arguments.of(new Date(1639137600000L), "10 груд. 2021"),
+            Arguments.of(new Date(1636545600000L), "10 лист. 2021"),
+            Arguments.of(new Date(1633863600000L), "10 жовт. 2021"),
+            Arguments.of(new Date(1631271600000L), "10 вер. 2021"),
+            Arguments.of(new Date(1628593200000L), "10 серп. 2021"),
+            Arguments.of(new Date(1625914800000L), "10 лип. 2021"),
+            Arguments.of(new Date(1623322800000L), "10 черв. 2021"),
+            Arguments.of(new Date(1620644400000L), "10 трав. 2021"),
+            Arguments.of(new Date(1618052400000L), "10 квіт. 2021"),
+            Arguments.of(new Date(1615377600000L), "10 бер. 2021"),
+            Arguments.of(new Date(1612958400000L), "10 лют. 2021")
         );
     }
 
